79c1fc9638696773b58cd3e167a44bbec638cdb8
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-03-06 19:29:51 +0100

    Add logs to tests

--- tests/testthat/test-connect.R
+++ tests/testthat/test-connect.R
@@ -1,6 +1,14 @@
 test_that("bvq_connect returns the correct credentials", {
-    withr::local_envvar(FORMR_PWD = Sys.getenv("FORMR_PWD"))
+    withr::local_envvar(FORMR_PWD = Sys.getenv("FORMR_PWD", unset = NA))
+    withr::local_envvar(GOOGLE_TOKEN = Sys.getenv("GOOGLE_TOKEN", unset = NA))
     
+    if (is.na(Sys.getenv("FORMR_PWD", unset = NA))) {
+        print("FORMR_PWD is NA")
+    }
+    
+    if (is.na(Sys.getenv("GOOGLE_TOKEN", unset = NA))) {
+        print("GOOGLE_TOKEN is NA")
+    }
     expect_true(
         bvq_connect(verbose = FALSE, password = Sys.getenv("FORMR_PWD")),
         "bvq_connect returns success code after loggin in"
