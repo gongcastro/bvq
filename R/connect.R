35c8d77603495d2628fdcf602313e820253b11e7
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-03-06 13:54:04 +0100

    Add Google token to secrets

--- R/connect.R
+++ R/connect.R
@@ -49,8 +49,10 @@ bvq_connect <- function(google_email = NULL,
     
     if (!is_key_google) {
         
+        google_token <- Sys.getenv("GOOGLE_TOKEN", unset = NA)
+        
         tryCatch(
-            suppressWarnings(gs4_auth(email = google_email)), 
+            suppressWarnings(gs4_auth(email = google_email, token = google_token)), 
             error = function(e){
                 cli_abort(
                     strwrap(
