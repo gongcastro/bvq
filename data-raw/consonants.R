706758bb2096a35d7d5e61e6681d02f76ac9f1c4
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-04-23 16:10:46 +0200

    Save datasets to fixtures folder for testing and remove IPA variables

--- data-raw/consonants.R
+++ data-raw/consonants.R
@@ -1,17 +1,18 @@
 ## code to prepare `phonemes` dataset goes here
 library(dplyr)
-library(ipa)
 library(readxl)
 
 # consonants
 consonants <- read_xlsx(system.file("extdata/consonants.xlsx", 
-                                    package = "bvqdev")) %>% 
+                                    package = "bvq")) %>% 
     rename(xsampa = phoneme) %>% 
     mutate(across(-xsampa, as.factor),
            across(c(place, place_2, manner, voicing), 
-                  function(x) strsplit(as.character(x), ", ")),
-           ipa = xsampa(xsampa)) %>% 
-    relocate(xsampa, ipa, place, place_2, manner, voicing)
+                  function(x) strsplit(as.character(x), ", "))) %>% 
+    relocate(xsampa, place, place_2, manner, voicing)
 
-# export pool database
-usethis::use_data(consonants, overwrite = TRUE)
+# export for future testing
+saveRDS(consonants, test_path("fixtures", "consonants.rds"))
+
+# export database
+usethis::use_data(consonants, overwrite = TRUE, internal = FALSE)
